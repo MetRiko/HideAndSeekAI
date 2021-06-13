f71fff1dc36363ab9f516460e132ded6c6c8bdf7
Anna Szachiewicz <anna.szachiewicz@gmail.com>
2021-06-13 21:41:17 +0200

    Add user

--- src/core/Game.js
+++ src/core/Game.js
@@ -3,12 +3,13 @@ import { p5 } from "./P5Core"
 
 import { Player } from "./Player/Player"
 
-const player = new Player(100, 100)
+let player = null
 
 export const setupP5 = (p5) => {
 
 	p5.setup = () => {
 		p5.createCanvas(1280, 720, p5.P2D)
+		player = new Player(600, 300)
 	}
 	p5.draw = () => {
 
@@ -18,11 +19,15 @@ export const setupP5 = (p5) => {
 		// render
 		p5.background(100)
 		player.render(p5)
-		drawCircle()
+		// drawCircle()
 	}
+
+	// p5.mouseClicked = (event) => {
+	// 	player.mouseClicked(event)
+	// }
 }
 
-const drawCircle = () => {
-	p5.fill(255, 0, 0)
-	p5.circle(p5.mouseX, p5.mouseY, 20.0)
-}
+// const drawCircle = () => {
+// 	p5.fill(255, 0, 0)
+// 	p5.circle(p5.mouseX, p5.mouseY, 20.0)
+// }
