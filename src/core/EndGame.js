ec111769c66047da5387e3377dd800866ab85173
Anna Szachiewicz <anna.szachiewicz@gmail.com>
2021-06-17 01:48:29 +0200

    Restart game

--- src/core/EndGame.js
+++ src/core/EndGame.js
@@ -1,9 +1,10 @@
 import { p5 } from "./P5Core"
 
 export class EndGame {
-    constructor(time, score) {
+    constructor(time, score, onSpacePressed) {
         this.time = time
         this.score = score
+        this.onSpacePressed = onSpacePressed
     }
 
     render() {
@@ -13,5 +14,12 @@ export class EndGame {
         p5.text("You got caught!", 640, 300)
         p5.text('Time: ' + this.time, 640, 360)
         p5.text('Score: ' + this.score, 640, 400)
+
+        p5.text('Press SPACE to start a new game!', 640, 460)
+        p5.keyPressed = () => {
+            if (p5.keyCode == 32) {
+                this.onSpacePressed()
+            }
+        }
     }
 }
